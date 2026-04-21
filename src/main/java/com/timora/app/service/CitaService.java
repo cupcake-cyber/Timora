f34f484062a58595a63ff851f2ebca29c459fcf7
cupcake_cyber <u23217150@utp.edu.pe>
2026-04-20 21:57:16 -0500

    Logica de Cita - Milagros Nunja

---
+++ src/main/java/com/timora/app/service/CitaService.java
@@ -0,0 +1,18 @@
+package com.timora.app.service;
+
+import com.timora.app.models.Cita;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CitaService {
+    List<Cita> listarTodas();
+
+    Optional<Cita> obtenerPorId(Integer id);
+
+    Cita guardar(Cita cita);
+
+    Cita actualizar(Integer id, Cita cita);
+
+    void eliminar(Integer id);
+}
