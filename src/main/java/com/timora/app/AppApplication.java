34c7342674b4c2218dec094bdc0d9d79b5212f99
Kelly Gutierrez <U23208824@utp.edu.pe>
2026-04-21 01:06:44 -0500

    Primer commit de mi nueva rama

--- src/main/java/com/timora/app/AppApplication.java
+++ src/main/java/com/timora/app/AppApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppApplication { // run
+public class AppApplication {
+	// run
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
