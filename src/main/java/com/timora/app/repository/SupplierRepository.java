98588a5e9fdf94ec5790ff4c28fa5044caa0b68f
Kelly Gutierrez <U23208824@utp.edu.pe>
2026-05-25 01:16:22 -0500

    Actualización de crud

---
+++ src/main/java/com/timora/app/repository/SupplierRepository.java
@@ -0,0 +1,10 @@
+package com.timora.app.repository;
+
+import com.timora.app.models.Supplier;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SupplierRepository extends JpaRepository<Supplier, Long> {
+
+}
