c26f6f5bb97620b9425c952903af65dc67073bc2
JhoelRm <jhoelhramirez923@gmail.com>
2026-05-28 02:16:21 -0500

    cambios en las propiedades

--- src/main/resources/ db/schema.sql
+++ src/main/resources/ db/schema.sql
@@ -226,9 +226,8 @@ CREATE TABLE notification (
     type VARCHAR(20),
     message TEXT,
     status VARCHAR(20),
-    sent_at DATETIME,
     target VARCHAR(255),
-
+    sent_at DATETIME,
     FOREIGN KEY (user_id) REFERENCES user(id)
 );
 
