0325f18ea1c697257f3752182ed755ad704f654b
JhoelRm <jhoelhramirez923@gmail.com>
2026-06-27 04:07:25 -0500

    Cambios generales

--- src/main/java/com/timora/app/service/impl/AuthorizationServiceImpl.java
+++ src/main/java/com/timora/app/service/impl/AuthorizationServiceImpl.java
@@ -1,43 +1,43 @@
-package com.timora.app.service.impl;
-
-import com.timora.app.service.AuthorizationService;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-@Service
-public class AuthorizationServiceImpl
-        implements AuthorizationService {
-
-    private final RolePermissionRepository rolePermissionRepository;
-
-    public AuthorizationServiceImpl(
-            RolePermissionRepository rolePermissionRepository
-    ) {
-        this.rolePermissionRepository = rolePermissionRepository;
-    }
-
-    @Override
-    public List<String> getPermissions(
-            Long userId,
-            Long supplierId
-    ) {
-
-        return rolePermissionRepository
-                .findPermissionCodesByUserAndSupplier(
-                        userId,
-                        supplierId
-                );
-    }
-
-    @Override
-    public boolean hasPermission(
-            Long userId,
-            Long supplierId,
-            String permission
-    ) {
-
-        return getPermissions(userId, supplierId)
-                .contains(permission);
-    }
-}
+//package com.timora.app.service.impl;
+//
+//import com.timora.app.service.AuthorizationService;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//
+//@Service
+//public class AuthorizationServiceImpl
+//        implements AuthorizationService {
+//
+//    private final RolePermissionRepository rolePermissionRepository;
+//
+//    public AuthorizationServiceImpl(
+//            RolePermissionRepository rolePermissionRepository
+//    ) {
+//        this.rolePermissionRepository = rolePermissionRepository;
+//    }
+//
+//    @Override
+//    public List<String> getPermissions(
+//            Long userId,
+//            Long supplierId
+//    ) {
+//
+//        return rolePermissionRepository
+//                .findPermissionCodesByUserAndSupplier(
+//                        userId,
+//                        supplierId
+//                );
+//    }
+//
+//    @Override
+//    public boolean hasPermission(
+//            Long userId,
+//            Long supplierId,
+//            String permission
+//    ) {
+//
+//        return getPermissions(userId, supplierId)
+//                .contains(permission);
+//    }
+//}
